04a551131ee7cd12e9bcd6aebc9c73f07efb5454
ziyoalgorithms <ziyodullokhonanvarov@gmail.com>
2023-02-07 13:10:09 +0500

    Some bugs fixed

--- eShop/eShop/settings/base.py
+++ eShop/eShop/settings/base.py
@@ -162,8 +162,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'studiovisual0077@gmail.com'
-EMAIL_HOST_PASSWORD = 'ackkfyihgqvoswsl'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 AUTHENTICATION_BACKENDS = [
